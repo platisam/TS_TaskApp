fdef9472cc9c7588445106bc9aea3777bc082f6e
platisam <platisam@gmail.com>
2023-09-23 15:33:08 +0200

    creating InputField component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,11 @@
+const InputField = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input__submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+export default InputField;
