c0fa12263d1d830c048f9dd479bcf9cebc6f94a3
platisam <platisam@gmail.com>
2023-09-23 15:21:38 +0200

    setting style for heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+    </div>
+  );
+};
 
 export default App;
