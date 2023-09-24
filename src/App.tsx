0c0a136499a9bdc538b3da0e3f6e70a3e48079c0
platisam <platisam@gmail.com>
2023-09-24 16:51:25 +0200

    adding model file with Todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   return (
     <div className="App">
