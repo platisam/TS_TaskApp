cb943fc1fa7c0ea75bc09125dd69017e20ae02d9
platisam <platisam@gmail.com>
2023-09-24 20:22:41 +0200

    setting useRef hook to InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
     </div>
   );
 };
